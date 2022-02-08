b11bb348eed5ceaa703eb8820848d357cf36952d
Sajjad Saharkhan <31157619+sajjadsaharkhan@users.noreply.github.com>
2022-02-08 17:28:03 +0330

    add env controller

--- dotnetexample/Controllers/HomeController.cs
+++ dotnetexample/Controllers/HomeController.cs
@@ -6,9 +6,22 @@ namespace dotnetexample.Controllers;
 [Route("[controller]")]
 public class HomeController : ControllerBase
 {
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public HomeController(IHostEnvironment hostEnvironment)
+    {
+        _hostEnvironment = hostEnvironment;
+    }
+
     [HttpGet("[action]")]
     public IActionResult Sample()
     {
         return Ok($"time is : {DateTime.Now.ToLongTimeString()}");
+    }
+
+    [HttpGet("[action]")]
+    public IActionResult GetEnv()
+    {
+        return Ok($"environment is : {_hostEnvironment.EnvironmentName}");
     }
 }
